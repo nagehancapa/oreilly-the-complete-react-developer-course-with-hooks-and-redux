e22a548fae953d441fee08a1499936c2d407cc20
nagehancapa <capanagehan@gmail.com>
2021-06-08 17:10:48 +0200

    added object

--- React/indecision-app/src/app.js
+++ React/indecision-app/src/app.js
@@ -1,7 +1,35 @@
 console.log("app.js is running");
 
+var app = {
+  title: "Indecision App",
+  subtitle: "This is some info",
+};
+
 //JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: "Nagehan",
+  age: 33,
+  location: "Netherlands",
+};
+
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
 
 var appRoot = document.getElementById("app");
 
