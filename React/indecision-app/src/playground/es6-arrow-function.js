02ceb45c78c836ad9a6e9295df71870080570ab3
nagehancapa <capanagehan@gmail.com>
2021-06-09 14:34:12 +0200

    added arrow function example

---
+++ React/indecision-app/src/playground/es6-arrow-function.js
@@ -0,0 +1,11 @@
+const square = function (x) {
+  return x * x;
+};
+
+const squareArrow = (x) => x * x;
+
+const getFirstName = (fullName) => fullName.split(" ")[0];
+
+console.log(squareArrow(8));
+
+console.log(getFirstName("Nagehan Capa"));
