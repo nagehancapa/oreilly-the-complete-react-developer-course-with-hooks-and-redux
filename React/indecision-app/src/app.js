5dc7b8560532a448cb6d9b1d3fd5676da939728f
nagehancapa <capanagehan@gmail.com>
2021-06-09 19:38:00 +0200

    added option with math.random

--- React/indecision-app/src/app.js
+++ React/indecision-app/src/app.js
@@ -22,6 +22,12 @@ const wipeArray = () => {
   render();
 };
 
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
 const appRoot = document.getElementById("app");
 
 const render = () => {
@@ -30,7 +36,9 @@ const render = () => {
       <h1>{app.title}</h1>
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What should I do?
+      </button>
       <button onClick={wipeArray}>Remove All</button>
       <ol>
         {app.options.map((input) => (
