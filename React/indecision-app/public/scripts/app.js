6f98284118d3e5da23498345fe0757f21174bb15
nagehancapa <capanagehan@gmail.com>
2021-06-09 11:04:57 +0200

    added conditional rendering example

--- React/indecision-app/public/scripts/app.js
+++ React/indecision-app/public/scripts/app.js
@@ -4,10 +4,10 @@ console.log("app.js is running");
 
 var app = {
   title: "Indecision App",
-  subtitle: "This is some info"
+  subtitle: "This is some info",
+  options: ["One", "Two"]
 };
 
-//JSX - JavaScript XML
 var template = React.createElement(
   "div",
   null,
@@ -16,10 +16,15 @@ var template = React.createElement(
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     "p",
     null,
     app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
   ),
   React.createElement(
     "ol",
@@ -43,26 +48,32 @@ var user = {
   location: "Netherlands"
 };
 
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
 var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    user.name
+    user.name ? user.name : "Anonymous"
   ),
-  React.createElement(
+  user.age && user.age >= 18 && React.createElement(
     "p",
     null,
     "Age: ",
     user.age
   ),
-  React.createElement(
-    "p",
-    null,
-    "Location: ",
-    user.location
-  )
+  getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
--- React/indecision-app/src/app.js
+++ React/indecision-app/src/app.js
@@ -3,13 +3,14 @@ console.log("app.js is running");
 var app = {
   title: "Indecision App",
   subtitle: "This is some info",
+  options: ["One", "Two"],
 };
 
-//JSX - JavaScript XML
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -23,11 +24,17 @@ var user = {
   location: "Netherlands",
 };
 
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 );
 
