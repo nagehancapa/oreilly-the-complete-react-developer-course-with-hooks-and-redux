8e0e5981ff1056fe28485bfcc0b0daff17172e1d
nagehancapa <capanagehan@gmail.com>
2021-06-10 11:04:34 +0200

    added es6 subclass example

--- React/indecision-app/public/scripts/app.js
+++ React/indecision-app/public/scripts/app.js
@@ -1,6 +1,12 @@
 "use strict";
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -30,12 +36,73 @@ var Person = function () {
   return Person;
 }();
 
+var Student = function (_Person) {
+  _inherits(Student, _Person);
+
+  function Student(name, age, major) {
+    _classCallCheck(this, Student);
+
+    var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: "hasMojor",
+    value: function hasMojor() {
+      return !!this.major; //empty string is falsy,
+      // if I use string with ! operator it is true if the string is empty,
+      // if there is a string it will false
+    }
+  }, {
+    key: "getDescription",
+    value: function getDescription() {
+      var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), "getDescription", this).call(this);
+
+      if (this.hasMojor()) {
+        description += " Their major is " + this.major + ".";
+      }
+
+      return description;
+    }
+  }]);
+
+  return Student;
+}(Person);
+
+var Traveller = function (_Person2) {
+  _inherits(Traveller, _Person2);
+
+  function Traveller(name, age, homelocation) {
+    _classCallCheck(this, Traveller);
+
+    var _this2 = _possibleConstructorReturn(this, (Traveller.__proto__ || Object.getPrototypeOf(Traveller)).call(this, name, age));
+
+    _this2.homelocation = homelocation;
+    return _this2;
+  }
+
+  _createClass(Traveller, [{
+    key: "getGreeting",
+    value: function getGreeting() {
+      var greeting = _get(Traveller.prototype.__proto__ || Object.getPrototypeOf(Traveller.prototype), "getGreeting", this).call(this);
+
+      if (this.homelocation) {
+        greeting += " I'm visiting from " + this.homelocation + ".";
+      }
+
+      return greeting;
+    }
+  }]);
+
+  return Traveller;
+}(Person);
+
 // this.name = name || "test"; => use name if it's exist if it's not use "test"
 
-var me = new Person("Nagehan Capa", 33);
+var me = new Traveller("Nagehan Capa", 33, "Eindhoven");
 console.log(me.getGreeting());
-console.log(me.getDescription());
 
-var other = new Person();
+var other = new Traveller();
 console.log(other.getGreeting());
-console.log(other.getDescription());
--- React/indecision-app/src/playground/es6-classes-1.js
+++ React/indecision-app/src/playground/es6-classes-1.js
@@ -11,12 +11,47 @@ class Person {
   }
 }
 
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMojor() {
+    return !!this.major; //empty string is falsy,
+    // if I use string with ! operator it is true if the string is empty,
+    // if there is a string it will false
+  }
+  getDescription() {
+    let description = super.getDescription();
+
+    if (this.hasMojor()) {
+      description += ` Their major is ${this.major}.`;
+    }
+
+    return description;
+  }
+}
+
+class Traveller extends Person {
+  constructor(name, age, homelocation) {
+    super(name, age);
+    this.homelocation = homelocation;
+  }
+  getGreeting() {
+    let greeting = super.getGreeting();
+
+    if (this.homelocation) {
+      greeting += ` I'm visiting from ${this.homelocation}.`;
+    }
+
+    return greeting;
+  }
+}
+
 // this.name = name || "test"; => use name if it's exist if it's not use "test"
 
-const me = new Person("Nagehan Capa", 33);
+const me = new Traveller("Nagehan Capa", 33, "Eindhoven");
 console.log(me.getGreeting());
-console.log(me.getDescription());
 
-const other = new Person();
+const other = new Traveller();
 console.log(other.getGreeting());
-console.log(other.getDescription());
