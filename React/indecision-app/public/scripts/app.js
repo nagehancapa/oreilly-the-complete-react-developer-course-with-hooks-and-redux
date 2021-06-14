cc0cdf06cdcf053950b1732096c8d5de904574c1
nagehancapa <capanagehan@gmail.com>
2021-06-14 15:21:01 +0200

    added local storage methods to saving and loading data

--- React/indecision-app/public/scripts/app.js
+++ React/indecision-app/public/scripts/app.js
@@ -29,12 +29,26 @@ var IndecisionApp = function (_React$Component) {
   _createClass(IndecisionApp, [{
     key: "componentDidMount",
     value: function componentDidMount() {
-      console.log("fetching data");
+      try {
+        var json = localStorage.getItem("options");
+        var options = JSON.parse(json);
+
+        if (options) {
+          this.setState(function () {
+            return { options: options };
+          });
+        }
+      } catch (e) {
+        // Do nothing at all
+      }
     }
   }, {
     key: "componentDidUpdate",
-    value: function componentDidUpdate() {
-      console.log("saving data");
+    value: function componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+        var json = JSON.stringify(this.state.options);
+        localStorage.setItem("options", json);
+      }
     }
   }, {
     key: "componentWillUnmount",
@@ -77,7 +91,7 @@ var IndecisionApp = function (_React$Component) {
 
       this.setState(function (prevState) {
         return {
-          options: prevState.options.concat([option])
+          options: prevState.options.concat(option)
         };
       });
     }
@@ -152,6 +166,11 @@ var Options = function Options(props) {
       { onClick: props.handleDeleteOptions },
       "Remove All"
     ),
+    props.options.length === 0 && React.createElement(
+      "p",
+      null,
+      "Please add an option to get started!"
+    ),
     props.options.map(function (option) {
       return React.createElement(Option, {
         key: option,
@@ -204,6 +223,9 @@ var AddOption = function (_React$Component2) {
       this.setState(function () {
         return { error: error };
       });
+      if (!error) {
+        e.target.elements.option.value = "";
+      }
     }
   }, {
     key: "render",
--- React/indecision-app/src/app.js
+++ React/indecision-app/src/app.js
@@ -11,11 +11,23 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log("fetching data");
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
   }
 
-  componentDidUpdate() {
-    console.log("saving data");
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
   }
 
   componentWillUnmount() {
@@ -46,7 +58,7 @@ class IndecisionApp extends React.Component {
     }
 
     this.setState((prevState) => ({
-      options: prevState.options.concat([option]),
+      options: prevState.options.concat(option),
     }));
   }
 
@@ -101,6 +113,9 @@ const Options = (props) => {
   return (
     <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {props.options.length === 0 && (
+        <p>Please add an option to get started!</p>
+      )}
       {props.options.map((option) => (
         <Option
           key={option}
@@ -141,6 +156,9 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({ error }));
+    if (!error) {
+      e.target.elements.option.value = "";
+    }
   }
   render() {
     return (
