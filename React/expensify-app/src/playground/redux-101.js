ccd73b62afbf36802f239ee3fa5cc1b511d555d5
nagehancapa <capanagehan@gmail.com>
2021-06-18 22:21:01 +0200

    added actions

--- React/expensify-app/src/playground/redux-101.js
+++ React/expensify-app/src/playground/redux-101.js
@@ -3,12 +3,20 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
+      };
+    case "SET":
+      return {
+        count: action.count,
       };
     case "RESET":
       return {
@@ -19,10 +27,13 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({
   type: "INCREMENT",
+  incrementBy: 5,
 });
 
 store.dispatch({
@@ -35,6 +46,14 @@ store.dispatch({
 
 store.dispatch({
   type: "DECREMENT",
+  decrementBy: 10,
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101,
+});
