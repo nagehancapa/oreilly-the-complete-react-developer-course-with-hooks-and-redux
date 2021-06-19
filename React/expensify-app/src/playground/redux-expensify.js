5b2e68632e64825836e077d57dc3824f336e8830
nagehancapa <capanagehan@gmail.com>
2021-06-19 16:56:58 +0200

    added multiple reducers

--- React/expensify-app/src/playground/redux-expensify.js
+++ React/expensify-app/src/playground/redux-expensify.js
@@ -1,4 +1,37 @@
 import { createStore, combineReducers } from "redux";
+
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date", // date or amount
+  startDate: undefined,
+  endDate: undefined,
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer,
+  })
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [
     {
