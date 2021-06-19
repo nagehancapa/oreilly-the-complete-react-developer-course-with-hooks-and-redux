7436b771ef3d6a60d3521f39a1499083a13ebff5
nagehancapa <capanagehan@gmail.com>
2021-06-19 10:56:57 +0200

    used some es6 array destructuring feature

--- React/expensify-app/src/playground/destructuring.js
+++ React/expensify-app/src/playground/destructuring.js
@@ -1,3 +1,5 @@
+// Object destructuring
+
 const person = {
   name: "Nagehan",
   age: 33,
@@ -25,3 +27,15 @@ const book = {
 
 const { name: publisherName = "Self-Published" } = book.publisher;
 console.log(publisherName);
+
+// Array destructuring
+
+const address = ["Fransebaan", "Eindhoven", "Netherlands", "5627"];
+const [, city1, country = "Turkey"] = address;
+
+console.log(`You are in ${city1} ${country}.`);
+
+const item = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
