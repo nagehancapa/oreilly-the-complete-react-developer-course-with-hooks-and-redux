c85c94a2a4bc6feefba81b9308f91f022c708b00
nagehancapa <capanagehan@gmail.com>
2021-06-22 14:59:08 +0200

    added expense form

--- React/expensify-app/src/components/Header.js
+++ React/expensify-app/src/components/Header.js
@@ -5,10 +5,10 @@ const Header = () => (
   <header>
     <h1>Expensify</h1>
     <NavLink to="/" activeClassName="is-active" exact={true}>
-      Dashboard
+      Dashboard{" "}
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
-      Create Expense
+      Create Expense{" "}
     </NavLink>
     <NavLink to="/help" activeClassName="is-active">
       Help
