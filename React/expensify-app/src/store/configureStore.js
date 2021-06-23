d443223d4104c614f447c061593bb81be9c6b8b7
nagehancapa <capanagehan@gmail.com>
2021-06-23 13:22:30 +0200

    results filtered by dates

--- React/expensify-app/src/store/configureStore.js
+++ React/expensify-app/src/store/configureStore.js
@@ -2,12 +2,17 @@ import { createStore, combineReducers } from "redux";
 import expensesReducer from "../reducers/expenses";
 import filtersReducer from "../reducers/filters";
 
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (x) => x;
+
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer,
-    })
+    }),
+    enhancer
   );
 
   return store;
