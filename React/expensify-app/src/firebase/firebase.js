a65797affae1732de57233184f42c9597f5fbb6a
nagehancapa <capanagehan@gmail.com>
2021-07-06 16:46:26 +0200

    added data handler

--- React/expensify-app/src/firebase/firebase.js
+++ React/expensify-app/src/firebase/firebase.js
@@ -15,24 +15,50 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
+// child_removed
+database.ref("expenses").on("child_removed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// child_changed
+database.ref("expenses").on("child_changed", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// child_added
+database.ref("expenses").on("child_added", (snapshot) => {
+  console.log(snapshot.key, snapshot.val());
+});
+
+// database.ref("expenses").on("value", (snapshot) => {
+//   const expenses = [];
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val(),
+//     });
+//   });
+//   console.log(expenses);
+// });
+
+// database
+//   .ref("expenses")
+//   .once("value")
+//   .then((snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val(),
+//       });
+//     });
+//     console.log(expenses);
+//   });
+
 database.ref("expenses").push({
   description: "Rent",
   note: "",
   amount: 109500,
-  createdAt: 976123498763,
-});
-
-database.ref("expenses").push({
-  description: "Phone bill",
-  note: "",
-  amount: 5900,
-  createdAt: 976123498763,
-});
-
-database.ref("expenses").push({
-  description: "Food",
-  note: "",
-  amount: 1200,
   createdAt: 976123498763,
 });
 
