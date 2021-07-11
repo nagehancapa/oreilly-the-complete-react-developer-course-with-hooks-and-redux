a5748ffc482f17030f3abf5eaf61f1ef69d9a626
nagehancapa <capanagehan@gmail.com>
2021-07-11 14:18:20 +0200

    added startEditExpense

--- React/expensify-app/src/actions/expenses.js
+++ React/expensify-app/src/actions/expenses.js
@@ -36,7 +36,7 @@ export const removeExpense = ({ id } = {}) => ({
 
 export const startRemoveExpense = ({ id } = {}) => {
   return (dispatch) => {
-    database
+    return database
       .ref(`expenses/${id}`)
       .remove()
       .then(() => {
@@ -50,6 +50,17 @@ export const editExpense = (id, updates) => ({
   id,
   updates,
 });
+
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database
+      .ref(`expenses/${id}`)
+      .update(updates)
+      .then(() => {
+        dispatch(editExpense(id, updates));
+      });
+  };
+};
 
 export const setExpenses = (expenses) => ({
   type: "SET_EXPENSES",
--- React/expensify-app/src/tests/components/EditExpensePage.test.js
+++ React/expensify-app/src/tests/components/EditExpensePage.test.js
@@ -3,15 +3,15 @@ import { shallow } from "enzyme";
 import { EditExpensePage } from "../../components/EditExpensePage";
 import expenses from "../fixtures/expenses";
 
-let editExpense, history, startRemoveExpense, wrapper;
+let startEditExpense, history, startRemoveExpense, wrapper;
 
 beforeEach(() => {
-  editExpense = jest.fn();
+  startEditExpense = jest.fn();
   history = { push: jest.fn() };
   startRemoveExpense = jest.fn();
   wrapper = shallow(
     <EditExpensePage
-      editExpense={editExpense}
+      startEditExpense={startEditExpense}
       history={history}
       startRemoveExpense={startRemoveExpense}
       expense={expenses[2]}
@@ -23,10 +23,13 @@ test("should render EditExpensePage correctly", () => {
   expect(wrapper).toMatchSnapshot();
 });
 
-test("should handle editExpense", () => {
+test("should handle startEditExpense", () => {
   wrapper.find("ExpenseForm").prop("onSubmit")(expenses[2]);
   expect(history.push).toHaveBeenLastCalledWith("/");
-  expect(editExpense).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+  expect(startEditExpense).toHaveBeenLastCalledWith(
+    expenses[2].id,
+    expenses[2]
+  );
 });
 
 test("should handle startRemoveExpense", () => {
