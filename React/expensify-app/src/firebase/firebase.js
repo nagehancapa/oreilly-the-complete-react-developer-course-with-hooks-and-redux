1b8c8b16f5c78d01ee5e3fa8d24c39e95932bd94
nagehancapa <capanagehan@gmail.com>
2021-07-17 17:23:13 +0200

    added style files

--- React/expensify-app/src/firebase/firebase.js
+++ React/expensify-app/src/firebase/firebase.js
@@ -1,4 +1,7 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
